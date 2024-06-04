420353934cd8694c002cd9c42453b9a90476c85e
liuwill <liuwei_will@qq.com>
2024-06-04 23:30:10 +0800

    feat: change top logo svg

--- ui/pages/Home.tsx
+++ ui/pages/Home.tsx
@@ -39,8 +39,8 @@ const Home = () => {
           >
             {
               config.meta.seo.enhancedDataEnabled ?
-                `${ config.chain.name } blockchain explorer` :
-                `${ config.chain.name } explorer`
+                `${ config.chain.name } Blockchain Explorer` :
+                `${ config.chain.name } Explorer`
             }
           </Heading>
           <Box display={{ base: 'none', lg: 'flex' }}>
